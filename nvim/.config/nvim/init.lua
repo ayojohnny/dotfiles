f8025bd6e402de0af6f8f2ac1b96397151236377
Johnny Dickson <johnnydicksoncode@gmail.com>
2026-04-08 23:15:21 -0500

    config(nvim): setup lsp and inline diagnostics
    Setup nvim-lspconfig for easier lsp management.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,1 +1,8 @@
 require('config.options')
+
+vim.pack.add({
+  { src = 'https://github.com/neovim/nvim-lspconfig' }
+})
+
+vim.lsp.enable({ "lua_ls", "ts_ls" })
+
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,4 +1,17 @@
+vim.g.mapleader = " "
+
+vim.o.termguicolors = true  -- enable 24-bit colors
+vim.o.tabstop = 4           -- how many spaces tab inserts
+vim.o.softtabstop = 4       -- how many spaces tab inserts
 vim.o.number = true
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.o.autoindent = true
+vim.o.scrolloff = 8
+vim.o.wrap = false
+vim.o.number = true
+vim.o.signcolumn = "yes" -- always show sign column
 vim.o.relativenumber = true
 vim.o.cursorline = true
-vim.o.shiftwidth = 4
+vim.diagnostic.config({ virtual_text = true }) -- inline diagonostics
