6d6e57c7b78a30017ac03799932b6a8ee7e20de0
Johnny Dickson <johnnydicksoncode@gmail.com>
2026-06-12 16:28:46 -0500

    config(nvim): add telescope and yazi

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,8 +1,12 @@
 require('config.options')
 
 vim.pack.add({
-  { src = 'https://github.com/neovim/nvim-lspconfig' }
-})
+  { src = 'https://github.com/neovim/nvim-lspconfig' },
+  { src = 'https://github.com/nvim-lua/plenary.nvim' },
+  { src = 'https://github.com/nvim-telescope/telescope.nvim' },
+  { src = 'https://github.com/nvim-telescope/telescope-fzf-native.nvim' },
+  { src = 'https://github.com/mikavilpas/yazi.nvim' }
+});
 
 vim.lsp.enable({ 
     "lua_ls",
