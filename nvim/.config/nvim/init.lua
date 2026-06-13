ace0c2b511610b935b2e97db9753290160643a36
Johnny Dickson <johnnydicksoncode@gmail.com>
2026-06-12 21:50:38 -0500

    config(nvim): add conform

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,14 +1,27 @@
 require('config.options')
+require('config.keymaps')
 
 vim.pack.add({
-  { src = 'https://github.com/neovim/nvim-lspconfig' },
-  { src = 'https://github.com/nvim-lua/plenary.nvim' },
-  { src = 'https://github.com/nvim-telescope/telescope.nvim' },
-  { src = 'https://github.com/nvim-telescope/telescope-fzf-native.nvim' },
-  { src = 'https://github.com/mikavilpas/yazi.nvim' }
+    { src = 'https://github.com/neovim/nvim-lspconfig' },
+    { src = 'https://github.com/nvim-lua/plenary.nvim' },
+    { src = 'https://github.com/nvim-telescope/telescope.nvim' },
+    { src = 'https://github.com/nvim-telescope/telescope-fzf-native.nvim' },
+    { src = 'https://github.com/mikavilpas/yazi.nvim' },
+    { src = 'https://github.com/stevearc/conform.nvim' }
 });
 
-vim.lsp.enable({ 
+
+vim.lsp.config("lua_ls", {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { "vim" }
+            }
+        }
+    }
+})
+
+vim.lsp.enable({
     "lua_ls",
     "ts_ls",
     "terraform-lsp",
@@ -17,3 +30,4 @@ vim.lsp.enable({
     "astro"
 })
 
+require('plugins.conform')
---
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,3 @@
+vim.keymap.set("n", "`", "<cmd>Yazi<CR>", {
+    desc = "Open Yazi"
+})
